dacef0ce8d4b92ce8919762f2402a14947d4bd57
ackraaij <a.c.kraaij@law.leidenuniv.nl>
2026-07-06 09:17:18 +0000

    Deploying to gh-pages from @ ackraaij/ackraaij.github.io@a0d9446bf10a0a63c92b22c73565517b7d92e398 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -483,6 +483,13 @@ ninja.data = [{
           window.open("https://orcid.org/0000-0002-6733-4713", "_blank");
         },
       },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=gd5SklsAAAAJ", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
